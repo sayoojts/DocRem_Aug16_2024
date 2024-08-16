66f58169da849d3280e97ab396a8e5cdcdc6923c
Sayooj <sayoojts87@gmail.com>
2024-08-16 21:08:59 +0530

    1st commit

---
+++ flaskblog/models.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+#from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+#from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
+import jwt
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
+from flask import current_app
+from flaskblog import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        expiration = datetime.utcnow() + timedelta(seconds=expires_sec)
+        token = jwt.encode(
+            {'user_id': self.id, 'exp': expiration},
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256'
+        )
+        return token.decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        try:
+            payload = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+            user_id = payload.get('user_id')
+            return user_id
+        except jwt.ExpiredSignatureError:
+            # Handle expired token case
+            return None
+        except jwt.InvalidTokenError:
+            # Handle invalid token case
+            return None
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
